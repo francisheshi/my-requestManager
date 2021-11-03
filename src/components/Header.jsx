414dfe29a7bc4b18f35ac147c371893452b4d3db
francisheshi <62473362+francisheshi@users.noreply.github.com>
2021-11-03 13:22:50 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,36 @@
-import { PropTypes } from "prop-types";
-import Button from "./Button";
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import Button from './Button';
 
-const Header = ({title, onAdd, }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
 
-    const onClick = (e) => {
-        console.log(e)
-    };
-
-    return ( 
-        <header className='header'>
-        <h1>{title}</h1>
-        {location.pathname === '/' && (
-          <Button
-            color={showAdd ? 'red' : 'green'}
-            text={showAdd ? 'Close' : 'Add'}
-            onClick={onAdd}
-          />
-        )}
-      </header>
-     );
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
 }
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-// CSS in JSX
+// CSS in JS
 // const headingStyle = {
-//     color: "red",
-//     backgroundColor: "black"
-// }
- 
+//   color: 'red',
+//   backgroundColor: 'black',
+// };
+
 export default Header;
